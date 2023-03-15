b0807db14496d7d180939e6451bad5bbf4ab5377
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-03-15 15:30:26 +0300

    Modify repositories

--- src/main/java/com/flight/reservation/flightreservation/repositories/NotificationRepository.java
+++ src/main/java/com/flight/reservation/flightreservation/repositories/NotificationRepository.java
@@ -2,5 +2,5 @@ package com.flight.reservation.flightreservation.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Passenger extends JpaRepository<Long, com.flight.reservation.flightreservation.entities.Passenger> {
+public interface NotificationRepository extends JpaRepository<Long, NotificationRepository> {
 }
