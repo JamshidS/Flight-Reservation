78a525bf5795e6c4f34180e43c413b41153e1488
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-03-20 22:39:13 +0300

    Create Country.class

--- src/main/java/com/flight/reservation/flightreservation/entities/Passenger.java
+++ src/main/java/com/flight/reservation/flightreservation/entities/Passenger.java
@@ -9,18 +9,31 @@ import java.util.List;
 @Entity
 @Table(name = "passenger")
 @Data
-public class Passenger extends User {
+public class Passenger{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    @Column(name = "name")
-    private String name;
     @Column(name = "passportnumber")
     private String passportNumber;
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "dataOfBirth")
+    private String dataOfBirth;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phonenumber")
+    private String phoneNumber;
 
     @ManyToMany(cascade = {CascadeType.MERGE})
     private List<Flight> flights;
+
+    @ManyToOne
+    private Country country;
 
 }
