51728113fec61eebc2788877818a2c9abbb47bd6
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-03-20 22:57:35 +0300

    create repository

---
+++ src/main/java/com/flight/reservation/flightreservation/repositories/BookingRecordRepository.java
@@ -0,0 +1,7 @@
+package com.flight.reservation.flightreservation.repositories;
+
+import com.flight.reservation.flightreservation.entities.BookingRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRecordRepository extends JpaRepository<Long, BookingRecord> {
+}
