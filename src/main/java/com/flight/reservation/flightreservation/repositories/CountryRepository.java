5b2ec690886e97a9855da58057bbdb75c238f9f2
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-03-21 00:23:40 +0300

    flight service modified

--- src/main/java/com/flight/reservation/flightreservation/repositories/CountryRepository.java
+++ src/main/java/com/flight/reservation/flightreservation/repositories/CountryRepository.java
@@ -3,5 +3,5 @@ package com.flight.reservation.flightreservation.repositories;
 import com.flight.reservation.flightreservation.entities.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CountryRepository extends JpaRepository<Long, Country> {
+public interface CountryRepository extends JpaRepository<Country,Long> {
 }
--- src/main/java/com/flight/reservation/flightreservation/repositories/PassengerRepository.java
+++ src/main/java/com/flight/reservation/flightreservation/repositories/PassengerRepository.java
@@ -2,5 +2,5 @@ package com.flight.reservation.flightreservation.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PassengerRepository extends JpaRepository<Long, com.flight.reservation.flightreservation.entities.Passenger> {
+public interface PassengerRepository extends JpaRepository<com.flight.reservation.flightreservation.entities.Passenger,Long> {
 }
