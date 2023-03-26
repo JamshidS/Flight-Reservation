3e6c10ebc26d95a4c56d0256f8412e85d7c8fc7d
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-03-27 02:00:14 +0300

    update flight service

--- src/main/java/com/flight/reservation/flightreservation/repositories/FlightRepository.java
+++ src/main/java/com/flight/reservation/flightreservation/repositories/FlightRepository.java
@@ -3,8 +3,15 @@ package com.flight.reservation.flightreservation.repositories;
 import com.flight.reservation.flightreservation.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 
     Flight findByFlightNumber(String flightNumber);
+    List<Flight> findFlightsByAirportName(String name);
+
+    List<Flight> findByDate(String date);
+
+    Flight findFlightByAirportName(String name);
 
 }
