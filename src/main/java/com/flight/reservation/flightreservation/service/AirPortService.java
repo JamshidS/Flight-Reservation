c421fd1838a0d60c4ef7be33da5d9431401e7985
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-04-06 00:37:02 +0300

    update airportservice

--- src/main/java/com/flight/reservation/flightreservation/service/AirPortService.java
+++ src/main/java/com/flight/reservation/flightreservation/service/AirPortService.java
@@ -1,6 +1,17 @@
 package com.flight.reservation.flightreservation.service;
 
+import com.flight.reservation.flightreservation.entities.Airport;
+
+import java.util.List;
+
 public interface AirPortService {
+    String addAirPort(Airport airport);
+    List<Airport> getAll();
+
+    Airport getAirportById(Long id);
+
+    String deleteAirportById(Long id);
+    Airport findAirportByName(String name);
 
 
 }
